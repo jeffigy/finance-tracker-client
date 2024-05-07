428773a7feb8a3d93d9923ae0e510f94c34b493a
jeffigy <ejeffydev@gmail.com>
2024-05-07 23:53:14 +0800

    added finance record form

--- src/pages/dashboard/financial-record-form.tsx
+++ src/pages/dashboard/financial-record-form.tsx
@@ -1,8 +1,22 @@
 const FinancialRecordForm = () => {
   return (
-    <div>
-      <form action="">
-        <div></div>
+    <form action="" className="max-w-xs p-5 border border-slate-700 rounded-md">
+      <label className="form-control w-full max-w-xs">
+        <div className="label">
+          <span className="label-text">Description:</span>
+        </div>
+        <input className="input input-bordered w-full max-w-xs" />
+      </label>
+      <label className="form-control w-full max-w-xs">
+        <div className="label">
+          <span className="label-text">Amount:</span>
+        </div>
+        <input className="input input-bordered w-full max-w-xs" />
+      </label>
+      <label htmlFor="form-control w-full max-w-xs">
+        <div className="label">
+          <span className="label-text">Category:</span>
+        </div>
         <select
           className="select select-bordered w-full max-w-xs"
           name=""
@@ -18,8 +32,26 @@ const FinancialRecordForm = () => {
           <option value="Entertainment">Entertainment</option>
           <option value="Other">Other</option>
         </select>
-      </form>
-    </div>
+      </label>
+      <label htmlFor="form-control w-full max-w-xs">
+        <div className="label">
+          <span className="label-text">Payment Method:</span>
+        </div>
+        <select
+          className="select select-bordered w-full max-w-xs"
+          name=""
+          id=""
+        >
+          <option disabled selected value="">
+            Select a Payment Method
+          </option>
+          <option value="Credit Card">Credit Card</option>
+          <option value="Cash">Cash</option>
+          <option value="Bank Transfer">Bank Transfer</option>
+        </select>
+      </label>
+      <button className="btn w-full mt-6">Submit</button>
+    </form>
   );
 };
 
