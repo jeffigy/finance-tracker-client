3ef08bd3c32a08e6d26136c958ac81c5b0bd9196
jeffigy <ejeffydev@gmail.com>
2024-05-07 23:42:31 +0800

    added tailwindcss and daisyUI

--- src/pages/dashboard/financial-record-form.tsx
+++ src/pages/dashboard/financial-record-form.tsx
@@ -1,5 +1,26 @@
 const FinancialRecordForm = () => {
-  return <div></div>;
+  return (
+    <div>
+      <form action="">
+        <div></div>
+        <select
+          className="select select-bordered w-full max-w-xs"
+          name=""
+          id=""
+        >
+          <option disabled selected value="">
+            Select a Category
+          </option>
+          <option value="Food">Food</option>
+          <option value="Rent">Rent</option>
+          <option value="Salary">Salary</option>
+          <option value="Utilities">Utilities</option>
+          <option value="Entertainment">Entertainment</option>
+          <option value="Other">Other</option>
+        </select>
+      </form>
+    </div>
+  );
 };
 
 export default FinancialRecordForm;
